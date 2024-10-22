e23a9ffe2350aa2825536fb589e7d54f47953a75
pavan <ajaygangadhara66@gmail.com>
2024-10-22 15:05:51 +0530

    modified

--- Calculator.java
+++ Calculator.java
@@ -5,9 +5,14 @@ public int add(int a,int b)
 int c=a+b;
 return c;
 }
-public static void main(String args[])
+public int square(int x)
+{
+int z=x*x;
+}
+public void main (String args[])
 {
 Calculator cal = new Calculator();
-System.out.println("The sum of two numbers is "+(cal.add(2,3)));
+System.out.println("the sum"+cal.add(2,3));
+System.out.println("squ"+cal.square(4));
 }
 }
